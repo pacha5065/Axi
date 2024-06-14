859561ba362074ac595f768cf3a6787ad6d9652b
pacha5065 <prashantha.e@gmail.com>
2024-06-14 13:56:22 +0530

    Update Deploy.ps1

--- Deploy.ps1
+++ Deploy.ps1
@@ -9,3 +9,7 @@ dotnet test
 docker build -t super-service-image .
 
 docker run -it --name super-service super-service-image
+
+#if you want to push to cloud you can follow the below steps:
+# docker logging <docker hub url> - followed by your creds to log in successfully.
+# docker push super-service-image
