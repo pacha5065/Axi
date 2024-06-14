05ce638502b08c47ff91c979e6e5dfb717a991a8
pacha5065 <prashantha.e@gmail.com>
2024-06-14 13:52:50 +0530

    Update Deploy.ps1

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,5 +1,11 @@
 #Builds the image by building the code and running the tests:
 
+cd .\super-service\src\
+dotnet build
+
+cd .\super-service\test
+dotnet test
+
 docker build -t super-service-image .
 
 docker run -it --name super-service super-service-image
